0e703577c92d9191f6363cdd8c2a61727b3b526a
CharlesOkechukwu <realcharlieok@gmail>
2025-07-01 22:21:27 +0100

    setup s3 bucket configuration and test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 const { redisClient } = require('./config/redis');
+const { testBucket } = require('./config/s3Config');
 
 
 const server = http.createServer(app);
@@ -15,6 +17,39 @@ redisClient.on('error', (err) => {
   console.error('Redis connection error:', err);
 });
 
-server.listen(PORT, () => {
+// gracefully shut down server
+process.on('SIGINT', async () => {
+  console.log('\nGracefully shutting down server...');
+
+  // Close the server
+  await server.close(() => {
+    console.log('HTTP server closed.');
+  });
+
+  // Close Redis connection
+  if (redisClient && redisClient.isReady) {
+    console.log('Redis connection closed.');
+    await redisClient.disconnect();
+  }
+
+  // close mongoDB connection
+  try {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed.');
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+  }
+
+  process.exit(0);
+})
+
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+  // Test the S3 bucket connection
+  try {
+    await testBucket();
+  } catch (error) {
+    console.error('S3 bucket test failed:', error);
+    process.exit(1); // Exit if the S3 bucket test fails
+  }
 });
